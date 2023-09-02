2442fda0163c32f7937d62cef22262782b9a2a5c
Ariq-Ikbar-Hakim <143861046+Ariq-Ikbar-Hakim@users.noreply.github.com>
2023-09-02 12:47:18 +0700

    Tugas Genap
    1. Tugas Menghitung volume sebuah balok
    3. Tugas Memasukan jari jari bola

---
+++ Tugas Genap 1.py
@@ -0,0 +1,5 @@
+a = input("panjang :")
+b = input("lebar :")
+c = input("tinggi :")
+d = int(a) * int(b) * int(c)
+print("hasil dari volume balok tersebut : " + str(d))
---
+++ Tugas Genap 3.py
@@ -0,0 +1,9 @@
+# tugas genap kedua
+jari = int(input("Masukan Jari-jari Bola : "))
+phi = 22 / 7
+
+luas = int(4 * phi * (jari * jari))
+volume = int((4 / 3) * phi * (jari * jari * jari))
+
+print("Luas Bola = ", luas)
+print("Volume Bola = ", volume)
