4d08469dcd67f9bd3fa2f8ffaecf018bd64cf885
Ariq-Ikbar-Hakim <143861046+Ariq-Ikbar-Hakim@users.noreply.github.com>
2023-09-03 07:36:48 +0700

    Tugas Genap 3.py

--- Tugas Genap 3.py
+++ Tugas Genap 3.py
@@ -1,10 +1,12 @@
 # tugas genap ketiga
-# mencari jari-jari bola menggunakan rumus r = √L : (4 x π).
+# (di urutan 8) rumus luas bola = 4 x π x r²
+# (di urutan 9) rumus volume bola = 4/3 x π x r³
+
 jari = int(input("Jari-jari Bola : "))
-phi = 22 / 7
+π = 22 / 7
 
-luas = int(4 * phi * (jari * jari))
-volume = int((4 / 3) * phi * (jari * jari * jari))
+luas = int(4 * π * (jari * jari))
+volume = int((4 / 3) * π * (jari * jari * jari))
 
-print("Luas Bola = ", luas)
-print("Volume Bola = ", volume)
+print("Luas Bola = ", +(luas))
+print("Volume Bola = ", +(volume))
