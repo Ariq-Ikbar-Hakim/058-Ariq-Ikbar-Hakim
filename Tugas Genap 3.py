150af33d676d194226004d7be12892bd8761b5c3
Ariq-Ikbar-Hakim <143861046+Ariq-Ikbar-Hakim@users.noreply.github.com>
2023-09-04 20:06:31 +0700

    Tugas Genap 3.py

--- Tugas Genap 3.py
+++ Tugas Genap 3.py
@@ -2,11 +2,11 @@
 # (di urutan 8) rumus luas bola = 4 x π x r²
 # (di urutan 9) rumus volume bola = 4/3 x π x r³
 
-jari = int(input("Jari-jari Bola : "))
-π = 22 / 7
+r = int(input("Jari-jari Bola : "))
+π = 3.14
 
-luas = int(4 * π * (jari * jari))
-volume = int((4 / 3) * π * (jari * jari * jari))
+luas = int(4 * π * (r * r))
+volume = int((4 / 3) * π * (r * r * r))
 
 print("Luas Bola = ", +(luas))
 print("Volume Bola = ", +(volume))
